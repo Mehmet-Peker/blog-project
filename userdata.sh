b50861a0525560cb97c231508d82550e327331fa
Mehmet-Peker <mehmetpeker0205@gmail.com>
2023-10-13 13:57:31 +0300

    userdata updated by adding sudo

--- userdata.sh
+++ userdata.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-apt-get update -y
-apt-get install git -y
-apt-get install python3 -y
+sudo apt-get update -y
+sudo apt-get install git -y
+sudo apt-get install python3 -y
 cd /home/ubuntu/
 TOKEN=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
 git clone https://$TOKEN@github.com/Mehmet-Peker/blog-project.git
 cd /home/ubuntu/blog-project
-apt install python3-pip -y
-apt-get install python3.7-dev libmysqlclient-dev -y
-pip3 install -r requirements.txt
+sudo apt install python3-pip -y
+sudo apt-get install python3.7-dev libmysqlclient-dev -y
+sudo pip3 install -r requirements.txt
 cd /home/ubuntu/blog-project/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
