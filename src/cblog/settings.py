69c10f1a5a40b8e2727116726f691108aaa62087
Mp <mehmetpeker0205@gmail.com>
2023-10-13 23:00:42 +0300

    env,settings,userdata updates

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -85,10 +85,10 @@ WSGI_APPLICATION = 'cblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'clarusblogdb', # database name in RDS is written here
+        'NAME': 'pekerdb', # database name in RDS is written here
         'USER': 'peker', # database master username in RDS is written here
         'PASSWORD': config('PASSWORD'),
-        'HOST': 'my-mysql-database.cd4o4lkqwl66.us-east-1.rds.amazonaws.com',  # database endpoint is written here
+        'HOST': 'my-database.cd4o4lkqwl66.us-east-1.rds.amazonaws.com',  # database endpoint is written here
         'PORT': '3306' # database port is written here
     }
 }
@@ -145,7 +145,7 @@ LOGIN_URL = "login"
 
 AWS_STORAGE_BUCKET_NAME = 'www.mehmetpeker.online' # please enter your s3 bucket name
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_REGION_NAME = "us-east-1" # please enter your s3 region 
+AWS_S3_REGION_NAME = "us-east-1" # please enter your s3 region
 AWS_DEFAULT_ACL = 'public-read'
 
 AWS_LOCATION = 'static'
